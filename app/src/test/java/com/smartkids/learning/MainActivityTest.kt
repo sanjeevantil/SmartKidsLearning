2c1bb7a11f194369240b1cfcd5460ec8c8b1b957
Sanjeev Antil <sanjeevantil@example.com>
2026-07-07 16:34:06 +0530

    Add MainActivityTest

---
+++ app/src/test/java/com/smartkids/learning/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.smartkids.learning
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.annotation.Config
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@Config(application = HiltTestApplication::class, sdk = [33])
+class MainActivityTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun launchMainActivity_doesNotCrash() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java)
+        activity.create().start().resume()
+        
+        // If it doesn't throw an exception, the test passes
+    }
+}
